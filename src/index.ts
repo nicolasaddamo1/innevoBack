4ab83942aa23881c225c5d0e736a779a684e403b
Nico <nicolasaddamo1@gmail.com>
2025-01-27 15:02:48 -0300

    added cors and configuration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import  express  from "express";
 import indexRoutes from "./routes/index";
+import cors from "cors";
+
+const corsOptions = {
+    origin: ['http://localhost:8001',], // Allow requests only from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies, if your application uses them
+    optionsSuccessStatus: 204, // Some legacy browsers (IE11) choke on 204
+    // headers: 'Content-Type, Authorization, Content-Length, X-Requested-With',
+};
 
 const app = express();
 const port = 3000;
+app.use(cors(corsOptions));
+
 app.use(indexRoutes);
 
 app.listen(port, () => {
